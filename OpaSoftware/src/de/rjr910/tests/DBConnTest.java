2b870566a9feabae2b47959e63849f31160230d8
R-Rueckert <rrueckert@avameo.com>
2014-07-16 21:43:39 +0200

    Datenbank H2 erstellt
    H2 Verbind hergestellt. H2 JAr zum BuildPath hinzugefügt

---
+++ OpaSoftware/src/de/rjr910/tests/DBConnTest.java
@@ -0,0 +1,20 @@
+package de.rjr910.tests;
+
+import java.sql.*;
+public class DBConnTest {
+    public static void main(String[] a)
+            throws Exception {
+        Class.forName("org.h2.Driver");
+        Connection conn = DriverManager.
+            getConnection("jdbc:h2:~/test", "sa", "");
+
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM DEUTSCH");
+        
+        while(rs.next()){
+        	System.out.println(rs.getString("ANSWER1"));
+        }
+        
+        conn.close();
+    }
+}
