9cc1b037a1101293716c410427a6200b00b77453
R-Rueckert <rrueckert@avameo.com>
2014-07-22 20:57:43 +0200

    Erstellen von Game Klassen

--- OpaSoftware/src/de/rjr910/audio/AudioManager.java
+++ OpaSoftware/src/de/rjr910/audio/AudioManager.java
@@ -8,13 +8,24 @@ import javafx.scene.media.MediaPlayer;
 public class AudioManager {
 
 	Media hit;
+	MediaPlayer mediaPlayer;
 
 	public void play(String sound) throws NoSoundAvailableException {
 
 		this.hit = new Media(new File(sound).toURI().toString());
-		MediaPlayer mediaPlayer = new MediaPlayer(hit);
+		this.mediaPlayer = new MediaPlayer(hit);
 		mediaPlayer.play();
+	
+			
+//		while(mediaPlayer.onEndOfMediaProperty() != null){
+//			System.out.println(mediaPlayer.onEndOfMediaProperty());
+//		}
+//		mediaPlayer.setOnStopped(value);
+//		while(mediaPlayer.getStatus() == Status.UNKNOWN ){
+//			System.out.println(mediaPlayer.getStatus());
+//		}
 
 	}
+	
 
 }
