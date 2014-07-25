a0c5366492bf8e7c26f2eed8af2d18e18e1a0bb6
R-Rueckert <rrueckert@avameo.com>
2014-07-25 10:13:56 +0200

    probably discard

--- OpaSoftware/src/de/rjr910/controller/Controller.java
+++ OpaSoftware/src/de/rjr910/controller/Controller.java
@@ -1,29 +1,13 @@
 package de.rjr910.controller;
 
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
-import javafx.event.ActionEvent;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-import de.rjr910.audio.AudioManager;
-import de.rjr910.audio.NoSoundAvailableException;
-import de.rjr910.database.DatabaseManager;
-import de.rjr910.games.MathGame;
 import de.rjr910.games.WordGame;
 import de.rjr910.games.utilitiesMath.NumFieldFX;
 
@@ -50,8 +34,8 @@ public class Controller implements Initializable {
 		WordGame words = new WordGame(image,btnAnswer1,btnAnswer2,btnAnswer3,btnAnswer4,txtMessage);
 		words.init();
 		
-		MathGame math = new MathGame(aufgabe,solution);
-		math.init();
+//		MathGame math = new MathGame(aufgabe,solution);
+//		math.init();
 			
 		
 
